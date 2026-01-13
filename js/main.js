9fb3612cfb7be716a9316cbd0251fab4de888383
ManuelAliquo <manuelaliquo2103@gmail.com>
2026-01-13 14:48:42 +0100

    added event-listener to btn + info log

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 const passengerKm = document.getElementById("passenger-km");
 const passengerAge = document.getElementById("passenger-age");
+const submitBtn = document.getElementById("submit-btn");
 
-console.log(passengerKm);
-console.log(passengerAge);
+submitBtn.addEventListener("click", function () {
+  console.log(passengerKm.value);
+  console.log(passengerAge.value);
+});
