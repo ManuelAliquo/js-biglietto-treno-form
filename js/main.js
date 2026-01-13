247f9dad7861bd790f1bf5faebf84f804db1c42d
ManuelAliquo <manuelaliquo2103@gmail.com>
2026-01-13 18:55:37 +0100

    Milestone 2:  added form element + printed values

--- js/main.js
+++ js/main.js
@@ -1,28 +1,48 @@
+// Form
+const passengerForm = document.getElementById("passenger-form");
+const passengerName = document.getElementById("passenger-name");
 const passengerKm = document.getElementById("passenger-km");
 const passengerAge = document.getElementById("passenger-age");
 const submitBtn = document.getElementById("submit-btn");
 
+// Ticket
+const printedName = document.getElementById("printed-name");
+const printedKm = document.getElementById("printed-km");
+const printedAge = document.getElementById("printed-age");
+const printedPrice = document.getElementById("printed-price");
+
 const pricePerKm = 0.21;
-const discount20 = 20;
-const discount40 = 40;
+const discountMinor = 20;
+const discountOver = 40;
 let ticketPrice;
+
+passengerForm.addEventListener("click", function (event) {
+  event.preventDefault();
+});
 
 // # Al click del bottone
 submitBtn.addEventListener("click", function () {
-  // - si stampano km ed età
-  console.log(`Km da percorrere: ${passengerKm.value}Km`);
-  console.log(`Range Età passeggero: ${passengerAge.value}`);
+  // - si stampano nome, km ed età
+  console.log(`Nome del Passeggero: ${passengerName.value}`);
+  printedName.innerText = `${passengerName.value}`;
+  console.log(`Distanza: ${passengerKm.value}Km`);
+  printedKm.innerText = `${passengerKm.value}Km`;
+  console.log(`Fascia d'Età: ${passengerAge.value}`);
 
   // - si stampa il prezzo e in caso applica lo sconto
-  if (passengerAge.value === "maggiorenne") {
+  if (passengerAge.value === "Maggiorenne") {
+    printedAge.innerText = passengerAge.value;
     ticketPrice = passengerKm.value * pricePerKm;
-  } else if (passengerAge.value === "minorenne") {
+  } else if (passengerAge.value === "Minorenne") {
+    printedAge.innerText = `${passengerAge.value}, Sconto -20%`;
     ticketPrice =
-      passengerKm.value * pricePerKm - (discount20 / 100) * (passengerKm.value * pricePerKm);
-  } else if (passengerAge.value === "over-65") {
+      passengerKm.value * pricePerKm - (discountMinor / 100) * (passengerKm.value * pricePerKm);
+  } else if (passengerAge.value === "Over-65") {
+    printedAge.innerText = `${passengerAge.value}, Sconto -40%`;
     ticketPrice =
-      passengerKm.value * pricePerKm - (discount40 / 100) * (passengerKm.value * pricePerKm);
+      passengerKm.value * pricePerKm - (discountOver / 100) * (passengerKm.value * pricePerKm);
   }
 
-  console.log(`Prezzo del biglietto: ${ticketPrice}€`);
+  console.log(`Prezzo del biglietto: ${ticketPrice.toFixed(2)}€`);
+  printedPrice.innerText = `${ticketPrice.toFixed(2)}€`;
 });
